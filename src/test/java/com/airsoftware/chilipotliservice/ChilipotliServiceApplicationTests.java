daf0521d3862cb4a1b5b24249506364d539d7273
= <=>
2022-11-11 19:46:38 -0600

    items and orders on DB

--- src/test/java/com/airsoftware/chilipotliservice/ChilipotliServiceApplicationTests.java
+++ src/test/java/com/airsoftware/chilipotliservice/ChilipotliServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.airsoftware.chilipotliservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ChilipotliServiceApplicationTests {
 
 	@Test
